db942d7da24b99c928cd21a29f85debeff64b4ec
Harry Lording <z5164744@unsw.edu.au>
2019-07-20 10:30:21 +1000

    before goal JSON tests

---
+++ src/unsw/dungeon/DungeonLoaderLoadGoalTest.java
@@ -0,0 +1,24 @@
+package unsw.dungeon;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DungeonLoaderLoadGoalTest {
+
+	@Test
+	public void testLoadGoal() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLoadSimple() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLoadComplex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
