69b65709cf86b0d9f9527359412f835a92139d25
Peter Nguyen <peter.nguyen1@student.unsw.edu.au>
2019-07-21 00:32:37 +1000

    Update TreasureGoal.java

--- src/unsw/dungeon/TreasureGoal.java
+++ src/unsw/dungeon/TreasureGoal.java
@@ -3,11 +3,28 @@ package unsw.dungeon;
 public class TreasureGoal implements GoalCondition {
 	
 	@Override
-	public boolean checkGoal(Dungeon D){
-		Entity treasure = D.getTreasure();
-		boolean currentGoalCondition = D.checkGoal();
-		if (D.getPlayerX() == treasure.getX() && D.getPlayerY() == treasure.getY())
-			currentGoalCondition = true;
-		return currentGoalCondition;
+	public boolean checkGoal(Dungeon D) {
+//		Entity treasure = D.getTreasure();
+//		boolean currentGoalCondition = D.checkGoal();
+//		if (D.getPlayerX() == treasure.getX() && D.getPlayerY() == treasure.getY())
+//			currentGoalCondition = true;
+//		return currentGoalCondition;
+		
+		if (D.getTile(D.getPlayerX(), D.getPlayerY()) instanceof Treasure) {
+			Treasure exactSpot = (Treasure) D.getTile(D.getPlayerX(), D.getPlayerY());
+			exactSpot.setPickedUp(true);
+		}
+		
+		for (Entity entity : D.getEntities()) {
+			if (entity instanceof Treasure) {
+				Treasure checkedTreasure = (Treasure) entity;
+//				System.out.println(checkedTreasure.isPickedUp());
+				if (checkedTreasure.isPickedUp() == false) {
+//					System.out.println();
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }  
